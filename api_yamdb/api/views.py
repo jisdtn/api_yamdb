924e6c1dfcac3c2c296b63a23fd9eb91183d2835
Стас Басов <stas.basov@whatever.com>
2023-05-26 20:03:59 +0300

    NewFile1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.filters import SearchFilter
+from api.mixins import ModelMixinSet
+from reviews.models import Genre, Title, Category
+from .serializers import (GenreSerializer, TitleCreateSerializer,
+                          TitleReadSerializer, CategorySerializer)
 
-# Create your views here.
+
+class CategoryViewSet(ModelMixinSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (HZ,)
+    filter_backends = (SearchFilter, )
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class GenreViewSet(ModelMixinSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (HZ,)
+    filter_backends = (SearchFilter,)
+    search_fields = ('name', )
+    lookup_field = 'slug'
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleReadSerializer
+    permission_classes = (HZ,)
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH',):
+            return TitleCreateSerializer
+        return TitleReadSerializer
